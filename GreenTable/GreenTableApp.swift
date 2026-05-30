1b762b9766b33f6796ed46f8a645b770737d8588
Aral Cay <aral.cay.27@dartmouth.edu>
2026-05-30 18:21:05 -0400

    Initial Commit

---
+++ GreenTable/GreenTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenTableApp.swift
+//  GreenTable
+//
+//  Created by Aral Cay on 5/30/26.
+//
+
+import SwiftUI
+
+@main
+struct GreenTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
